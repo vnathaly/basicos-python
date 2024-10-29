308d83f8e4199d86f4685f2c391e000931f28194
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-10-29 17:11:55 -0300

    Renders

---
+++ Flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+#'''El parametro de name, es para decirle a la app que esta siendo importada y que sepa exactamente donde buscar los archivos html y dem[as]'''
+app = Flask(__name__)
+
+#Esto del @ son decoradores
+@app.route("/")
+def index():
+  name = "Nath"
+  num = 2
+  return render_template("index.html", name = name, num = num)
+
