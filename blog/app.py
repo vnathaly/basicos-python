209efe02de7497e6f0fe323fe9f38913a1a1423f
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-11-05 17:08:11 -0300

    Added a blog

---
+++ blog/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/Nath/Desktop/python/blog/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+  __tablename__ = "posts"
+  id = db.Column(db.Integer, primary_key=True)
+  titulo = db.Column(db.String, nullable=False)
+  fecha = db.Column(db.DateTime, default=datetime.now)
+  texto = db.Column(db.String, nullable=False)
+
+
