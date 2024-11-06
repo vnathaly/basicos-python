cf64210a4cf5ea9f05e0426aa1d134166c4a50c1
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-11-06 12:23:55 -0300

    Explication about ORM

--- orm/app.py
+++ orm/app.py
@@ -7,3 +7,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Users/Nath/Desktop/python/or
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+app.app_context().push()
+
+class Equipo(db.Model):
+	__tablename__ = "posts"
+	id = db.Column(db.Integer, primary_key=True)
+	nombre = db.Column(db.String, nullable=False)
+	fecha = db.Column(db.DateTime, default=datetime.now)
+
+def __repr__(self):
+	return "<Equipo %r>" % self.nombre
+
+if __name__ == "__main__":
+	app.run(debug=True)
